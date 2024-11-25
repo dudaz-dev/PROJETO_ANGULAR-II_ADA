dc10265f1c45732ddf3c3ad4d7e0c89e223c5a31
dudaz-dev <dudaz.dev@gmail.com>
2024-11-25 17:50:17 -0300

    new page

--- gerenciadorConsultas/src/app/app.routes.ts
+++ gerenciadorConsultas/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AboutUsMainComponent } from './modules/about-us-main/about-us-main.component';
+import { LoginComponent } from './modules/auth/components/login/login.component';
+import { RegisterComponent } from './modules/auth/components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'about-us-main',
+    pathMatch: 'full',
+  },
+  {
+    path: 'about-us-main',
+    component: AboutUsMainComponent,
+  },
+  {
+    path:'login',
+    component: LoginComponent,
+  },
+  {
+    path:'register',
+    component: RegisterComponent,
+  }
+];
