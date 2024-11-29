1f0e7357897716313d7ec2438ca4300653fbcbc1
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-29 17:12:32 -0300

    Atualiza formulário e busca o cep no componente

--- gerenciadorConsultas/src/app/modules/auth/components/register/register.component.ts
+++ gerenciadorConsultas/src/app/modules/auth/components/register/register.component.ts
@@ -16,8 +16,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NgxMaskDirective } from 'ngx-mask';
+import { AuthService } from '../../services/auth.service';
+import { ViaCepService } from '../../services/via-cep.service';
 
 @Component({
   selector: 'app-register',
@@ -39,23 +41,37 @@ import { NgxMaskDirective } from 'ngx-mask';
 export class RegisterComponent implements OnInit {
   protected form: FormGroup = new FormGroup(
     {
-      name: new FormControl(null, [Validators.required]),
-      email: new FormControl(null, [Validators.required]),
-      password: new FormControl(null, [
+      name: new FormControl(null, [
         Validators.required,
-        this.validatePassword,
+        Validators.minLength(3),
       ]),
+      documentNumber: new FormControl(null, Validators.required),
+      phone: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      zipCode: new FormControl(null, [Validators.required]),
+      street: new FormControl({ value: null, disabled: true }),
+      number: new FormControl(null, [Validators.required]),
+      neighborhood: new FormControl({ value: null, disabled: true }),
+      state: new FormControl({ value: null, disabled: true }),
+      city: new FormControl({ value: null, disabled: true }),
+      password: new FormControl(null, [this.validatePassword]),
       passwordConfirm: new FormControl(null, [Validators.required]),
       role: new FormControl({ disabled: true, value: 'USER' }),
     },
     {
-      validators: matchPassword,
+      validators: matchPasswordValidator,
     }
   );
 
   protected roleValue = this.form.controls['role'].value;
+  protected errorMessage: string = '';
 
-  constructor(private destroyRef: DestroyRef) {}
+  constructor(
+    private authService: AuthService,
+    private viaCepService: ViaCepService,
+    private router: Router,
+    private destroyRef: DestroyRef
+  ) {}
 
   ngOnInit(): void {
     this.form.controls['role'].valueChanges
@@ -67,7 +83,20 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(): void {
     this.form.controls['role'].enable();
-    console.log(this.form);
+    const payload = this.form.getRawValue();
+    this.authService
+      .register(payload)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        complete: () => {
+          console.log('Usuário cadastrado!');
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+    this.form.controls['role'].disable();
   }
 
   protected changeRoleToAdmin() {
@@ -84,6 +113,41 @@ export class RegisterComponent implements OnInit {
     this.form.controls['role'].disable();
   }
 
+  protected fetchAddress(): void {
+    const zipCodeValue = this.form.controls['zipCode'].getRawValue();
+
+    if (!this.viaCepService.isValidZipCode(zipCodeValue)) {
+      this.errorMessage = 'CEP inválido.';
+      return;
+    }
+
+    this.viaCepService
+      .getAddressByZipCode(zipCodeValue)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (data) => {
+          this.handleAddressResponse(data);
+        },
+        error: (err) => {
+          console.error('Erro ao buscar o CEP:', err);
+          this.errorMessage = 'Erro ao consultar o CEP.';
+        },
+      });
+  }
+
+  private handleAddressResponse(data: any) {
+    if (data.erro) {
+      this.errorMessage = 'CEP não encontrado.';
+    } else {
+      this.form.patchValue({
+        street: data.logradouro,
+        neighborhood: data.bairro,
+        state: data.uf,
+        city: data.localidade,
+      });
+    }
+  }
+
   private validatePassword({
     value,
   }: FormControlState<string>): { [key: string]: boolean } | null {
@@ -97,11 +161,11 @@ export class RegisterComponent implements OnInit {
   }
 }
 
-export const matchPassword: ValidatorFn = (
+export const matchPasswordValidator: ValidatorFn = (
   control: AbstractControl
 ): ValidationErrors | null => {
-  let password = control.get('password');
-  let passwordConfirm = control.get('passwordConfirm');
+  const password = control.get('password');
+  const passwordConfirm = control.get('passwordConfirm');
 
   if (
     password &&
