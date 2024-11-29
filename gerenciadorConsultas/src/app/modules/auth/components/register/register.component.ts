988e8a3d00a1e8fc93bb279c12a950d82295bdab
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-28 23:10:00 -0300

    Adiciona validadores personalizados para o formulário

--- gerenciadorConsultas/src/app/modules/auth/components/register/register.component.ts
+++ gerenciadorConsultas/src/app/modules/auth/components/register/register.component.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, DestroyRef, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import {
+  AbstractControl,
   FormControl,
   FormControlState,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -34,13 +37,21 @@ import { NgxMaskDirective } from 'ngx-mask';
   styleUrl: './register.component.css',
 })
 export class RegisterComponent implements OnInit {
-  protected form: FormGroup = new FormGroup({
-    name: new FormControl(null, [Validators.required]),
-    email: new FormControl(null, [Validators.required]),
-    password: new FormControl(null, [Validators.required]),
-    passwordConfirm: new FormControl(null, [Validators.required]),
-    role: new FormControl({ disabled: true, value: 'USER' }),
-  });
+  protected form: FormGroup = new FormGroup(
+    {
+      name: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [
+        Validators.required,
+        this.validatePassword,
+      ]),
+      passwordConfirm: new FormControl(null, [Validators.required]),
+      role: new FormControl({ disabled: true, value: 'USER' }),
+    },
+    {
+      validators: matchPassword,
+    }
+  );
 
   protected roleValue = this.form.controls['role'].value;
 
@@ -67,4 +78,33 @@ export class RegisterComponent implements OnInit {
     this.form.controls['role'].setValue(role);
     this.form.controls['role'].disable();
   }
+
+  private validatePassword({
+    value,
+  }: FormControlState<string>): { [key: string]: boolean } | null {
+    if (!value) return { requiredPassword: true };
+
+    if (!value.trim()) return { emptyPassword: true };
+
+    if (value.length < 5) return { minLengthPassword: true };
+
+    return null;
+  }
 }
+
+export const matchPassword: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  let password = control.get('password');
+  let passwordConfirm = control.get('passwordConfirm');
+
+  if (
+    password &&
+    passwordConfirm &&
+    password?.value !== passwordConfirm?.value
+  ) {
+    return { passwordMatchError: true };
+  }
+
+  return null;
+};
