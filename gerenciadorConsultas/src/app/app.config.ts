7af50f1b15a1edb5d2c84e66cefa77331f971b8e
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-29 17:10:21 -0300

    Adiciona o provide HttpClient no app.config

--- gerenciadorConsultas/src/app/app.config.ts
+++ gerenciadorConsultas/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
 import { routes } from './app.routes';
@@ -9,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
     provideEnvironmentNgxMask(),
   ],
 };
