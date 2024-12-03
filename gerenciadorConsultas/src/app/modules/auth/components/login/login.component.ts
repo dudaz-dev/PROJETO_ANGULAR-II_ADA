21880738ff88b247a673b089f9f66d47d360c4aa
zarkio42 <pedro.contato24@gmail.com>
2024-12-03 14:32:29 -0300

    feat/comecando a estrutura da tela de login

--- gerenciadorConsultas/src/app/modules/auth/components/login/login.component.ts
+++ gerenciadorConsultas/src/app/modules/auth/components/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  AbstractControl,
+  FormControl,
+  FormControlState,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { Router } from '@angular/router';
+import { NgxMaskDirective } from 'ngx-mask';
+import { AuthService } from '../../services/auth.service';
+import { ViaCepService } from '../../services/via-cep.service';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    CommonModule,
+    HeaderComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  users = [
+    { name: '', image: 'assets/user1.png' },
+    { name: '', image: 'assets/user2.png' },
+    { name: '', image: 'assets/user3.png' }
+  ];
 
 }
