42a9604c7a748de1a394e0aa2d6df1d8f461cfa4
zarkio42 <pedro.contato24@gmail.com>
2024-12-05 19:43:42 -0300

    feat/adicionando flags de verificação de erros com material

--- gerenciadorConsultas/src/app/modules/auth/components/login/login.component.ts
+++ gerenciadorConsultas/src/app/modules/auth/components/login/login.component.ts
@@ -13,6 +13,9 @@ import {
 } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
@@ -22,6 +25,9 @@ import { HeaderComponent } from '../header/header.component';
     ReactiveFormsModule,
     CommonModule,
     HeaderComponent,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -32,6 +38,37 @@ export class LoginComponent {
 
   onRoleChange(newRole: string) {
     this.currentRole = newRole;
+  }
+
+  loginForm = new FormGroup({
+    login: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    senha: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Faça algo com os dados do formulário
+      console.log(this.loginForm.value);
+      // Redirecionar ou tratar o login
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
+
+  get login() {
+    return this.loginForm.get('login');
+  }
+
+  get senha() {
+    return this.loginForm.get('senha');
   }
 
 }
