ef3bcbd0aa622302f552f0816339d511f92b3017
dudaz-dev <dudaz.dev@gmail.com>
2024-12-16 12:18:45 -0300

    '' ajustado página peincipal adm''

--- gerenciadorConsultas/src/app/modules/aplicacao/components/consultations/consultations.component.ts
+++ gerenciadorConsultas/src/app/modules/aplicacao/components/consultations/consultations.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, DestroyRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Observable } from 'rxjs'; // Importando Observable
+import { MatCardModule } from '@angular/material/card';
+import { Observable } from 'rxjs';
 import { ConsultasService } from '../../services/consultas.service';
 import { Appointments } from '../../model/appointments.model';
 
 @Component({
   selector: 'app-consultations',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './consultations.component.html',
   styleUrls: ['./consultations.component.css'],
 })
